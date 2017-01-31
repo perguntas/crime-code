34eeac54d420c28c08613c7a7430d8d2bedfe02f
perguntas <lvomfell@gmail.com>
2017-01-31 13:39:11 +0000

    update crimecleaning

--- initialcode.r
+++ initialcode.r
@@ -4,10 +4,21 @@
 #                         #
 #-------------------------#
 
-setwd("D:")
+setwd("D:/crime-data")
 
-crime16 <- read.csv("NYPD_Complaint_Data_Current_YTD_16.csv", stringsAsFactors = F)
+crime16 <- read.csv("NYPD_Complaint_Map_Year_to_Date16.csv", stringsAsFactors = F)
 
 head(crime16)
+
+colnames(crime16) <- c("cmpl_num", "fromDate", "fromTime", "toDate", "toTime", "reportDate", "KY_CD", "offense", "PD_CD",
+                      "PD_DESC", "CRM_ATPT_CPTD_CD", "Category", "Jurisdiction", "Boro", "ADDR_PCT_CD", 
+                      "LocDesc", "Location", "PARKS_NM", "HADEVELOPT", "X_Coord", "Y_Coord", "Latitude", "Longitude", "Lat_Lon")
+
+# formatting the date columns
+crime16$fromDate   <- format(as.Date(crime16$fromDate, "%m/%d/%Y"), "%Y-%m-%d")
+crime16$toDate     <- format(as.Date(crime16$toDate, "%m/%d/%Y"), "%Y-%m-%d")
+crime16$reportDate <- format(as.Date(crime16$reportDate, "%m/%d/%Y"), "%Y-%m-%d")
+
+
 
 
