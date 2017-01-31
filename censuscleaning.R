497ce00997d60dbabce0de30e0108f0b7d1792f9
perguntas <lvomfell@gmail.com>
2017-01-31 16:08:51 +0000

    init censuscleaning

--- censuscleaning.R
+++ censuscleaning.R
@@ -8,13 +8,27 @@ setwd("D:/crime-data")
 
 census <- read.csv("2010_NYC_Census_Tracts.csv", stringsAsFactors = F)
 
+# Remove all columns related to absolute numbers, excluding housing numbers and median age, excluding the first occurrence
 census[1,] <- tolower(census[1,])
-
-# Remove all columns without content (denoted by " ( X ) " in the census file)
-remove <- colnames(census)[grepl("( X )", census[2,])]
+remove <- colnames(census)[(grepl("number", census[1,]) & !grepl("hous|median|relationship", census[1,]))][-1]
 census <- census[, !colnames(census) %in% remove]
 
-# Remove all columns related to absolute number, excluding housing numbers and median age, excluding the first occurrence
-remove <- colnames(census)[(grepl("number", census[1,]) & !grepl("hous|median", census[1,]))][-1]
+remove <- colnames(census)[(grepl("percent", census[1,]) & grepl("occupancy|household", census[1,]))]
 census <- census[, !colnames(census) %in% remove]
 
+# remove all tenure columns, keep HD02_S181, HD02_S184
+remove <- colnames(census)[grepl("tenure", census[1,])]
+remove <- remove[!(remove %in% c("HD02_S181", "HD02_S184"))]
+census <- census[, !colnames(census) %in% remove]
+
+# delete explanative first line
+census <- census[-1,]
+
+# Remove all columns with only 100 (percent) or " ( X ) " (ie without content)
+census <- Filter(function(x) (length(unique(x)) > 2), census)
+
+
+# to do:
+# - remove tracts not in NYC
+# - remove lines with minimal variation
+# - correlation analysis?
